7011ca3240bbb85547515e671f8c0c0daed0348a
hamin <daniel1009040904@gmail.com>
2026-03-15 21:14:58 +0900

    adsense fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,16 +31,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* Adsense Script */}
+
         <Script
-          id="adsense-script"
           async
+          strategy="afterInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2379721968959562"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
         />
 
         {children}
+
       </body>
     </html>
   );
