69d241b0aa2189521998a424a7954f69bb446660
hamin <daniel1009040904@gmail.com>
2026-03-15 20:56:31 +0900

    Add AdSense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -16,8 +17,8 @@ export const metadata: Metadata = {
   title: "중고거래 수익 계산기",
   description: "중고거래, 마진, ROI, 손익분기 계산기",
   verification: {
-    google: "yPSQW2oDKqsSyPJ8LEBczTxH"
-  }
+    google: "yPSQW2oDKqsSyPJ8LEBczTxH",
+  },
 };
 
 export default function RootLayout({
@@ -26,7 +27,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
+      <head>
+        <Script
+          id="adsense-script"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2379721968959562"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
